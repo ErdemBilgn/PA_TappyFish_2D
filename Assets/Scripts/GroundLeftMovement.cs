9324f2e70c228942918fe4c41dbdab62d4bd5553
Wise2nd <mr.erdembilgin@gmail.com>
2023-05-29 20:15:33 +0300

    Added ObstacleSpawner

--- Assets/Scripts/GroundLeftMovement.cs
+++ Assets/Scripts/GroundLeftMovement.cs
@@ -8,11 +8,19 @@ public class GroundLeftMovement : MonoBehaviour
     [SerializeField] float _speed;
     BoxCollider2D box;
     float groundWidth;
+    float obstacleWidth;
 
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+        if(gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidth = box.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
 
@@ -20,10 +28,15 @@ public class GroundLeftMovement : MonoBehaviour
     {
         transform.position = new Vector2(transform.position.x - _speed * Time.deltaTime, transform.position.y);
 
-        if(transform.position.x <= -groundWidth)
+        if(gameObject.CompareTag("Ground"))
         {
-            transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+            if (transform.position.x <= -groundWidth)
+            {
+                transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+            }
         }
+
+
 
     }
 }
